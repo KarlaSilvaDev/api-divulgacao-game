2331d1e51f37c53f06f2cf22495f39df23636f1c
Nathalia Porcelli <nathalia_porcelli@hotmail.com>
2023-10-28 18:01:09 -0300

    atualização db_utils para usar o banco

--- db_utils.py
+++ db_utils.py
@@ -1,75 +1,90 @@
 from flask import render_template
-from data import personagens, usuarios
-from datetime import datetime
+import sqlite3
 
 
-def gerar_id(dicionario: dict):
-    lista_de_chaves = list(dicionario.keys())
-
-    if lista_de_chaves:
-        id = lista_de_chaves[-1] + 1
-        print(id)
-    else:
-        id = 1
-
-    return id
-
+def gerar_id_personagens():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT seq  FROM sqlite_sequence WHERE name='personagens'")
+    next_id = cursor.fetchone()[0]
+    return next_id + 1
 
 # CREATE - CRIAR
-def adicionar_personagem(
-    nome, classe, força, destreza, inteligência, idade, história, imagem
-):
-    personagens[gerar_id(personagens)] = {
-        "nome": nome,
-        "classe": classe,
-        "força": força,
-        "destreza": destreza,
-        "inteligência": inteligência,
-        "idade": idade,
-        "história": história,
-        "imagem": imagem,
-    }
 
 
-def adicionar_usuario(nome, login, email, senha):
-    usuarios[gerar_id(usuarios)] = {
-        "nome": nome,
-        "login": login,
-        "email": email,
-        "senha": senha,
-    }
+def adicionar_personagem(nome, classe, forca, destreza, inteligencia, idade, habilidade, historia, imagem):
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        sql_insert = "INSERT INTO personagens (nome, classe, forca, destreza, inteligencia, idade, habilidade, historia, imagem) values (?,?,?,?,?,?,?,?,?)"
+        cursor.execute(sql_insert, (nome, classe, forca, destreza,
+                       inteligencia, idade, habilidade, historia, imagem))
+        id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+        return id
+    except Exception as ex:
+        print(ex)
+        return 0
+
+# retorna um único personagem
 
 
-def listar(dicionario: dict):  # READ - LER
-    return dicionario
+def retornar_personagem(id: int):
+    try:
+        if id == 0:
+            return gerar_id_personagens(), "", "", "", "", "", "", "", "", ""
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
 
+        sql_select = "SELECT * FROM personagens WHERE id = ?"
+        cursor.execute(sql_select, (id, ))
+        id, nome, classe, forca, destreza, inteligencia, idade, habilidade, historia, imagem = cursor.fetchone()
+        conn.close()
+        return id, nome, classe, forca, destreza, inteligencia, idade, habilidade, historia, imagem
+    except:
+        return False
 
-def detalhar_por_id(id: int, dicionario: dict):  # READ - LER
-    if id in dicionario.keys():
-        return dicionario[id]
-    else:
-        return {}
+# atualizar personagem
+def atualizar_personagem(id: int, nome, classe, forca, destreza, inteligencia, idade, habilidade, historia, imagem):  # UPDATE - ATUALIZAR
+    try:
+        # tentar atualizar
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        sql_update = "UPDATE personagens SET nome = ?, classe = ?, forca = ?, destreza = ?, inteligencia = ?, idade = ?, habilidade = ?, historia = ?, imagem = ? WHERE id = ?"
+        cursor.execute(sql_update, (nome, classe, forca, destreza,
+                       inteligencia, idade, habilidade, historia, imagem, id))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
 
+# excluir personagem
+def remover_personagem(id: int):
+    try:
+        # tentar atualizar
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        sql_delete = "DELETE FROM personagens WHERE id = ?"
+        cursor.execute(sql_delete, (id, ))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
 
-def atualizar(id: int, dicionario: dict, dados_editados: dict):  # UPDATE - ATUALIZAR
-    dicionario[id].update(dados_editados)
-
-
-def remover(id: int, dicionario: dict):  # DELETE - DELETAR
-    del dicionario[id]
-
-
-def tratar_iso_para_dmy(data: str):  # TRATAMENTO DE DATAS - DMY = DIA, MÊS E ANO
-    if "-" in data:
-        data = datetime.strptime(data, "%Y-%m-%d")
-        return data.strftime("%d/%m/%Y")
-    else:
-        return data
-
-
-def tratar_dmy_para_iso(data: str):  # TRATAMENTO DE DATAS - DMY = DIA, MÊS E ANO
-    if "/" in data:
-        data = datetime.strptime(data, "%d/%m/%Y")
-        return data.strftime("%Y-%m-%d")
-    else:
-        return data
+# retorna todos os personagens
+def retornar_personagens():
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        sql_select = "SELECT * FROM personagens"
+        cursor.execute(sql_select)
+        personagens = cursor.fetchall()
+        conn.close()
+        return personagens
+    except:
+        return False
