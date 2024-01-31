a894581a0ebfded964ea1a20f404b91dccbf4c25
Nathalia Porcelli <nathalia_porcelli@hotmail.com>
2024-01-30 23:05:33 -0300

    Ajustes botão ver mais card

--- static/js/personagens.js
+++ static/js/personagens.js
@@ -1,49 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var botoesVerMais = document.querySelectorAll('.button-ver-mais');
+  var botoesVerMais = document.querySelectorAll('.button-ver-mais');
+  var modal = document.getElementById("modal");
+  var textoCompletoElement = document.getElementById("texto-completo");
 
-    botoesVerMais.forEach(function (botao) {
+  botoesVerMais.forEach(function (botao) {
       botao.addEventListener("click", function () {
-        var card = botao.closest(".card");
-        var textoResumido = card.querySelector(".resumo-texto");
-        var textoCompleto = card.querySelector(".completo-texto");
+          var card = botao.closest(".card");
+          var textoCompleto = card.querySelector(".completo-texto").textContent;
 
-        if (textoResumido.style.display === "none" || textoResumido.style.display === "") {
-          // Se o texto resumido está oculto, exibe e muda o texto do botão
-          textoResumido.style.display = "block";
-          textoCompleto.style.display = "none";
-          botao.textContent = "Ver Mais";
-        } else {
-          // Se o texto resumido está visível, oculta e muda o texto do botão
-          textoResumido.style.display = "none";
-          textoCompleto.style.display = "block";
-          botao.textContent = "Ver Menos";
-        }
+          // Preenche o conteúdo da modal com o texto completo
+          textoCompletoElement.textContent = textoCompleto;
+
+          // Exibe a modal
+          modal.style.display = "block";
       });
-    });
+  });
+
+  // Fecha a modal quando o usuário clica no botão de fechar
+  var closeButton = document.getElementsByClassName("close")[0];
+  closeButton.addEventListener("click", function () {
+      modal.style.display = "none";
+  });
+
+  // Fecha a modal quando o usuário clica fora dela
+  window.addEventListener("click", function (event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  });
 });
-  
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
